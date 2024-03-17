739e9d19c16ddac144fd7f2e4dc87c3ac9796b50
엔터프라이즈IT개발팀/차선호 <1131182@device.autoever.com>
2024-03-18 08:59:16 +0900

    apply WebSecurityConfig

--- src/main/java/com/theono/securitywithjwt/config/WebSecurityConfig.java
+++ src/main/java/com/theono/securitywithjwt/config/WebSecurityConfig.java
@@ -1,14 +1,48 @@
 package com.theono.securitywithjwt.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
+        //csrf disable
+        http
+                .csrf((csrf) -> csrf.disable());
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((formLogin) -> formLogin.disable());
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((basic) -> basic.disable());
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
